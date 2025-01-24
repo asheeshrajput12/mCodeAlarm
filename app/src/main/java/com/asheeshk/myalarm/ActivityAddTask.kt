8c70b061f0e948e0fa24fba099e8b1d95baf575a
Asheesh Kumar <asheeshkumar75497@gmail.com>
2025-01-25 00:47:45 +0530

    resign bottom sheet item @composable

--- app/src/main/java/com/asheeshk/myalarm/ActivityAddTask.kt
+++ app/src/main/java/com/asheeshk/myalarm/ActivityAddTask.kt
@@ -9,6 +9,8 @@ import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -30,9 +32,14 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -57,6 +64,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
@@ -75,6 +83,7 @@ import androidx.compose.ui.unit.sp
 import com.asheeshk.myalarm.ui.theme.MyAlarmTheme
 import com.asheeshk.myalarm.ui.theme.Pink10
 import com.asheeshk.myalarm.ui.theme.Pink80
+import com.asheeshk.myalarm.ui.theme.Purple80
 import kotlinx.coroutines.launch
 import kotlin.math.abs
 
@@ -152,7 +161,6 @@ fun ExerciseAlarmScreen(userName: String = "Akshay") {
                 )
             }
         }
-
         // Welcome Text
         Text(
             text = "Welcome $userName,",
@@ -161,7 +169,6 @@ fun ExerciseAlarmScreen(userName: String = "Akshay") {
             textAlign = TextAlign.Start,
             color = Color(0xFF6A1B9A) // Purple color
         )
-
         Text(
             text = "what time do you want to exercise?",
             modifier = Modifier
@@ -170,7 +177,35 @@ fun ExerciseAlarmScreen(userName: String = "Akshay") {
             fontSize = 24.sp,
             textAlign = TextAlign.Start
         )
-
+        val timeList = (0..23).flatMap { hour ->
+            (0..59).map { minute ->
+                Pair(hour, minute)
+            }
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(110.dp), // Adjust height to control visible items
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(timeList) { time ->
+                Row {
+                    Text(
+                        text = "${time.first.toString().padStart(2, '0')}",
+                        style = MaterialTheme.typography.headlineLarge,
+                        color = Color.Black,
+                        fontWeight = FontWeight(700)
+                    )
+                    Spacer(modifier = Modifier.width(30.dp))
+                    Text(
+                        text = "${time.second.toString().padStart(2, '0')}",
+                        style = MaterialTheme.typography.headlineLarge,
+                        color = Color.Black,
+                        fontWeight = FontWeight(700)
+                    )
+                }
+            }
+        }
         // Time Picker
         Row(
             modifier = Modifier
@@ -342,7 +377,7 @@ fun ShowAlarmRingtoneBottomSheet(
                                 ringtone.play()
                                 // Return selected item
                                 // selectItem = ringtone.getTitle(context)
-                              //  onRingtoneSelected(alarmRingtones.indexOf(ringtone), ringtone.first)
+                                //  onRingtoneSelected(alarmRingtones.indexOf(ringtone), ringtone.first)
                                 showBottomSheet = false
                             }
                             .padding(16.dp)
@@ -452,11 +487,13 @@ fun ShowBottomSheetModal() {
     // Bottom Sheet
     if (showBottomSheet) {
         ModalBottomSheet(
+            modifier = Modifier.background(Color.White),
+            contentColor = Color.White,
             onDismissRequest = { showBottomSheet = false }, // Dismiss on outside tap
             sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
         ) {
             BottomSheetContent(
-                items = listOf("Push Up", "Stretching", "Pulling","Dumble","Jumping","Running","Tradil"),
+                items = listOf("Push Up", "Stretching", "Pulling","Dumbbell","Jumping","Running","Treadmill","Legs","Shoulder","Cardio"),
                 onItemSelected = { item ->
                     selectedItem=item // Pass result back to parent
                     showBottomSheet = false // Close the bottom sheet
@@ -465,28 +502,91 @@ fun ShowBottomSheetModal() {
         }
     }
 }
-
 @Composable
 fun BottomSheetContent(items: List<String>, onItemSelected: (String) -> Unit) {
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
     ) {
-        Text(
-            text = "Select an Option",
-            style = TextStyle(fontSize = 20.sp, color = Color.Black),
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
-        items.forEach { item ->
+        item {
+            Text(
+                text = "Select an Option",
+                style = TextStyle(fontSize = 20.sp, color = Color.Black),
+                modifier = Modifier.padding(bottom = 10.dp)
+            )
+        }
+        items(items) { item ->
+            CardActivityView(item)
+            // Uncomment if using Text instead of custom view
+            /*
             Text(
                 text = item,
                 style = MaterialTheme.typography.bodyMedium,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clickable { onItemSelected(item) } // Call the callback with the selected item
+                    .clickable { onItemSelected(item) }
                     .padding(vertical = 12.dp)
+            )
+            */
+        }
+    }
+}
+
+@Composable
+fun CardActivityView(txtActivityName:String="Your Text Here"){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 5.dp, bottom = 5.dp, start = 0.dp, end = 0.dp),
+        colors = CardColors(containerColor = Color.White, contentColor = Color.Black, disabledContainerColor = Color.White, disabledContentColor = Color.Black),
+        shape = RoundedCornerShape(16.dp), // Rounded corners
+        border = BorderStroke(0.4.dp, Color.Gray) // Stroke color and width
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            // Image on the left
+            Card( modifier = Modifier
+                .wrapContentWidth()
+                .wrapContentHeight(),
+                colors = CardColors(containerColor = Pink10, contentColor = Color.Black, disabledContainerColor = Color.White, disabledContentColor = Color.Black),
+                shape = RoundedCornerShape(10.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_skipping), // Replace with your image
+                    contentDescription = "Image",
+                    modifier = Modifier
+                        .size(28.dp)
+                        .padding(5.dp) // Optional: Make the image circular
+                )
+            }
+
+            Column( modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 16.dp)) {
+                Text(
+                    text = txtActivityName,
+                    style = MaterialTheme.typography.labelLarge,
+                    color = Color.Black
+                )
+                Text(
+                    text = "minimum for 10 minutes",
+                    style = MaterialTheme.typography.bodySmall,
+                    fontFamily = FontFamily.Cursive,
+                    color =  Color(0xFF8B878F)
+                )
+            }
+            Image(
+                painter = painterResource(id = R.drawable.ic_arrow_square_right), // Replace with your image
+                contentDescription = "Image",
+                modifier = Modifier
+                    .size(22.dp)
+                    .clickable {  }
+                    .clip(RectangleShape) // Optional: Make the image circular
             )
         }
     }
